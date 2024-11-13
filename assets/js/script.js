3e7509fbfb36125d6e1cc9b05914954c715b7555
Alex Andhrei P. Indiongco <112994129+AlexIndiongco@users.noreply.github.com>
2024-11-13 14:26:09 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+
+    themeToggle.addEventListener("change", () => {
+        if (themeToggle.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+        } else {
+            document.documentElement.removeAttribute("data-theme");
+        }
+    });
+});
+
